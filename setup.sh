e6216e5ff9aa79f3d1678ae029533203f0b90d55
root <root@mel-debian>
2015-12-01 22:11:01 -0800

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -10,14 +10,14 @@ echo ""
 echo "Enabling CGI in Apache"
 	a2enmod cgi
 echo "Copying lamphtml.sh to /usr/lib/cgi-bin/"
-	cp weblamp/cgi-bin/lamphtml.sh /usr/lib/cgi-bin/lamphtml.sh
-	cp weblamp/cgi-bin/iframe.txt /usr/lib/cgi-bin/iframe.txt
-	cp weblamp/cgi-bin/iframe.html /usr/lib/cgi-bin/iframe.html
+	cp weblamp/apache2/cgi-bin/lamphtml.sh /usr/lib/cgi-bin/lamphtml.sh
+	cp weblamp/apache2/cgi-bin/iframe.txt /usr/lib/cgi-bin/iframe.txt
+	cp weblamp/apache2/cgi-bin/iframe.html /usr/lib/cgi-bin/iframe.html
 echo "Copying lampctl to /usr/local/bin/"
-	cp weblamp/lampctl /usr/local/bin/lampctl
+	cp weblamp/apache2/lampctl /usr/local/bin/lampctl
 echo ""
 echo "Copying configuration file to /etc/apache2/apache2.conf"
-	cp weblamp/etc/apache2.conf /etc/apache2/apache2.conf
+	cp weblamp/apache2/etc/apache2.conf /etc/apache2/apache2.conf
 echo "Changing permissions of /usr/lib/cgi-bin/"
 	chmod 777 /usr/lib/cgi-bin/
 	chmod 777 /usr/lib/cgi-bin/*
