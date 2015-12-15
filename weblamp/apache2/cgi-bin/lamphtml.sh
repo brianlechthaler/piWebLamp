b9ac7983d878cea7655f61b0c08d959b78cd1011
root <root@mel-debian>
2015-12-14 22:02:34 -0800

    Updated lamphtml.sh

--- weblamp/apache2/cgi-bin/lamphtml.sh
+++ weblamp/apache2/cgi-bin/lamphtml.sh
@@ -75,12 +75,7 @@ function cgi_getvars()
 }
 function html ()
 	{
-	echo '<html>
-		<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title></title> 
-		</head>
-		</html>'
+	echo '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title></title></head></html>'
 	cgi_getvars BOTH ALL
 		if [ "$MODE" != "" ]
 			then
