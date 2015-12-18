c61fae1371b3a7dec2fad2ec681bbedde90998b6
root <root@mel-debian>
2015-12-18 15:20:33 -0800

    Updated status_comet.php

--- weblamp/apache2/cgi-bin/status_comet.php
+++ weblamp/apache2/cgi-bin/status_comet.php
@@ -4,18 +4,36 @@ use Lamp\Comet;
 use Lamp\Gpio;
 $Comet = new Comet;
 $GPIO = new Gpio;
-$pin_list = $GPIO->getHackablePins();
-while (!($Comet->checkForUpdates('status.txt'))) {
-	$status = array();
+while (!($Comet->checkForUpdates('status.json'))) {
+	$pin_list=array(2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 22, 23, 24, 25, 27);
+	$status = array
+	(
+		array('2','none','0'),
+		array('3','none','0'),
+		array('4','none','0'),
+		array('7','none','0'),
+		array('8','none','0'),
+		array('9','none','0'),
+		array('10','none','0'),
+		array('11','none','0'),
+		array('14','none','0'),
+		array('15','none','0'),
+		array('17','none','0'),
+		array('18','none','0'),
+		array('22','none','0'),
+		array('23','none','0'),
+		array('24','none','0'),
+		array('25','none','0'),
+		array('27','none','0')
+	);
 	foreach ($pin_list as $pin){
-		file_put_contents('status.txt',"");
 		if ($GPIO->isExported) {
-			$status[$pin].direction = $GPIO->currentDirection($pin));
+			$status[$pin][1] = $GPIO->currentDirection($pin);
 		} else {
-			$status[$pin].direction = 'None';
+			$status[$pin][1] = 'None';
 		}
-		$status[$pin].value = $GPIO->input($pin);
-		file_put_contents('status.txt', $pin.','.$status[$pin].value .','.$status[$pin].value .'/n', FILE_APPEND);
+		$status[$pin][2] = $GPIO->input($pin);
+		file_put_contents('status.json',json_encode($status));
 	}
 }
 $Comet->response($status);
