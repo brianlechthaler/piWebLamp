b36e95a881ca368cd0bf95306e64a65f2d9a1a95
root <root@mel-debian>
2015-12-18 15:34:11 -0800

    Updated CometInterface.php

--- weblamp/apache2/cgi-bin/src/Lamp/CometInterface.php
+++ weblamp/apache2/cgi-bin/src/Lamp/CometInterface.php
@@ -2,7 +2,7 @@
 namespace Lamp;
 interface CometInterface
 {
-	public function checkForUpdates(last_mod_time, $file);
+	public function checkForUpdates($last_mod_time, $file);
 	public function response($response);
 }
 ?>
