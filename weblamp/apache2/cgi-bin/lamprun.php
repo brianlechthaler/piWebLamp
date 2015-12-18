bfb47bd4560bb874b35ccb262efdcbcc301c02cd
root <root@mel-debian>
2015-12-17 20:33:59 -0800

    Updated lamprun.php

--- weblamp/apache2/cgi-bin/lamprun.php
+++ weblamp/apache2/cgi-bin/lamprun.php
@@ -4,11 +4,11 @@ use Lamp\Lamp;
 echo(exec('whoami'));
 $get = unserialize($argv[1]);
 $lamp = new Lamp;
-$lamp->get = $get;
-$lamp->mode = $get['mode'];
-$lamp->pin = (boolean) $get['pin'];
-$lamp->morse = $get['morse'];
-$lamp->show_sleep_time = $get['show_sleep_time'];
-$lamp->verbose = $get['verbose'];
+$lamp->get = (array) $get;
+$lamp->mode = (string) $get['mode'];
+$lamp->pin = (int) $get['pin'];
+$lamp->morse = (boolean) $get['morse'];
+$lamp->show_sleep_time = (boolean) $get['show_sleep_time'];
+$lamp->verbose = (boolean) $get['verbose'];
 $lamp->mode_select();
 ?>
