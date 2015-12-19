733317e45fe7cd02d4fac76c1e47c92498cdd527
root <root@mel-debian>
2015-12-18 21:07:02 -0800

    Updated status_comet.php

--- weblamp/apache2/cgi-bin/status_comet.php
+++ weblamp/apache2/cgi-bin/status_comet.php
@@ -3,8 +3,8 @@ require 'vendor/autoload.php';
 use Lamp\Gpio;
 $pin_list = array(2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 22, 23, 24, 25, 27); 
 $status = file_get_contents('/var/www/html/status.json');
-$status = json_decode($status);
-while ($current_mod_time <= $status.last_mod_time]) {
+$status = json_decode($status,  true);
+while ($current_mod_time <= $status['last_mod_time']) {
 	global $status, $pin_list;
 	$Gpio = new Gpio;
 	foreach ($pin_list as $pin) {
