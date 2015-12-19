5baf6bd3e8065e528b8348f02e3ecd4015442042
root <root@mel-debian>
2015-12-18 19:26:07 -0800

    Updated status_comet.js

--- weblamp/apache2/html/status_comet.js
+++ weblamp/apache2/html/status_comet.js
@@ -1,41 +1,43 @@
 function setRow(tableId, rowId, colNum, newValue) {
     $('#'+tableId).find('tr#'+rowId).find('td:eq(colNum)').html(newValue);
 }
-function handleResponse(data) {
-		setRow("odd_pins", 3, 3, data[2][1]);
-		setRow("odd_pins", 3, 4, data[2][2]);
-		setRow("odd_pins", 3, 3, data[3][1]);
-		setRow("odd_pins", 3, 4, data[3][2]);
-		setRow("odd_pins", 3, 3, data[4][1]);
-		setRow("odd_pins", 3, 4, data[4][2]);
-		setRow("even_pins", 3, 3, data[7][1]);
-		setRow("even_pins", 3,  4, data[7][2]);
-		setRow("even_pins", 3,  3, data[8][1]);
-		setRow("even_pins", 3,  4, data[8][2]);
-		setRow("odd_pins", 3, 3, data[9][1]);
-		setRow("odd_pins", 3, 4, data[9][2]);
-		setRow("odd_pins", 3, 3, data[10][1]);
-		setRow("odd_pins", 3, 4, data[10][2]);
-		setRow("odd_pins", 3, 3, data[11][1]);
-		setRow("odd_pins", 3, 4, data[11][2]);
-		setRow("even_pins", 3, 3, data[14][1]);
-		setRow("even_pins", 3, 4, data[14][2]);
-		setRow("even_pins", 3, 3, data[15][1]);
-		setRow("even_pins", 3, 4, data[15][2]);
-		setRow("odd_pins", 3, 3, data[17][1]);
-		setRow("odd_pins", 3, 4, data[17][2]);
-		setRow("even_pins", 3, 3, data[18][1]);
-		setRow("even_pins", 3, 4, data[18][2]);
-		setRow("odd_pins", 3, 3, data[22][1]);
-		setRow("odd_pins", 3, 4, data[22][2]);
-		setRow("even_pins", 3, 3, data[23][1]);
-		setRow("even_pins", 3, 4, data[23][2]);
-		setRow("even_pins", 3, 3, data[24][1]);
-		setRow("even_pins", 3, 4, data[24][2]);
-		setRow("even_pins", 3, 3, data[25][1]);
-		setRow("even_pins", 3, 4, data[25][2]);
-		setRow("odd_pins", 3, 3, data[27][1]);
-		setRow("odd_pins", 3, 4, data[27][2]);
+function handleResponse(response) {
+	var data;
+	data = JSON.parse(response);
+	setRow("odd_pins", 3, 3, data[2][1]);
+	setRow("odd_pins", 3, 4, data[2][2]);
+	setRow("odd_pins", 3, 3, data[3][1]);
+	setRow("odd_pins", 3, 4, data[3][2]);
+	setRow("odd_pins", 3, 3, data[4][1]);
+	setRow("odd_pins", 3, 4, data[4][2]);
+	setRow("even_pins", 3, 3, data[7][1]);
+	setRow("even_pins", 3,  4, data[7][2]);
+	setRow("even_pins", 3,  3, data[8][1]);
+	setRow("even_pins", 3,  4, data[8][2]);
+	setRow("odd_pins", 3, 3, data[9][1]);
+	setRow("odd_pins", 3, 4, data[9][2]);
+	setRow("odd_pins", 3, 3, data[10][1]);
+	setRow("odd_pins", 3, 4, data[10][2]);
+	setRow("odd_pins", 3, 3, data[11][1]);
+	setRow("odd_pins", 3, 4, data[11][2]);
+	setRow("even_pins", 3, 3, data[14][1]);
+	setRow("even_pins", 3, 4, data[14][2]);
+	setRow("even_pins", 3, 3, data[15][1]);
+	setRow("even_pins", 3, 4, data[15][2]);
+	setRow("odd_pins", 3, 3, data[17][1]);
+	setRow("odd_pins", 3, 4, data[17][2]);
+	setRow("even_pins", 3, 3, data[18][1]);
+	setRow("even_pins", 3, 4, data[18][2]);
+	setRow("odd_pins", 3, 3, data[22][1]);
+	setRow("odd_pins", 3, 4, data[22][2]);
+	setRow("even_pins", 3, 3, data[23][1]);
+	setRow("even_pins", 3, 4, data[23][2]);
+	setRow("even_pins", 3, 3, data[24][1]);
+	setRow("even_pins", 3, 4, data[24][2]);
+	setRow("even_pins", 3, 3, data[25][1]);
+	setRow("even_pins", 3, 4, data[25][2]);
+	setRow("odd_pins", 3, 3, data[27][1]);
+	setRow("odd_pins", 3, 4, data[27][2]);
 }
 var url = '/cgi-bin/status_comet.php';
 var noerror = true;
@@ -47,9 +49,9 @@ type: 'get',
 data: {},
 success: function(transport) {
 // handle the server response
-eval('var data = '+transport);
+eval('var response = '+transport);
 
-handleResponse(data);
+handleResponse(response);
 noerror = true;
 }, 
 complete: function(transport) {
