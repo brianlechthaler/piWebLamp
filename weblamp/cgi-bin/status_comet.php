7dda5724c08200742a56809c3ccae4ce5fe8448a
root <root@mel-debian>
2015-12-31 23:11:06 -0800

    Updated status_comet.php

--- weblamp/cgi-bin/status_comet.php
+++ weblamp/cgi-bin/status_comet.php
@@ -20,41 +20,40 @@ function readStatus(){
 	}
 }
 function comet() {
-	$status = $_SESSION['status'];
+	if(!array_key_exists('status', $_SESSION)) {
+		$_SESSION['status'] = array
+		(
+			'pin_2' =>array('pin_2' ,'None','N/A'),
+			'pin_3' =>array('pin_3' ,'None','N/A'),
+			'pin_4' =>array('pin_4' ,'None','N/A'),
+			'pin_7' =>array('pin_7' ,'None','N/A'),
+			'pin_8' =>array('pin_8' ,'None','N/A'),
+			'pin_9' =>array('pin_9' ,'None','N/A'),
+			'pin_10'=>array('pin_10','None','N/A'),
+			'pin_11'=>array('pin_11','None','N/A'),
+			'pin_14'=>array('pin_14','None','N/A'),
+			'pin_15'=>array('pin_15','None','N/A'),
+			'pin_17'=>array('pin_17','None','N/A'),
+			'pin_18'=>array('pin_18','None','N/A'),
+			'pin_22'=>array('pin_22','None','N/A'),
+			'pin_23'=>array('pin_23','None','N/A'),
+			'pin_24'=>array('pin_24','None','N/A'),
+			'pin_25'=>array('pin_25','None','N/A'),
+			'pin_27'=>array('pin_27','None','N/A')
+		);
+	}
 	while (true) {
 		readStatus();
 		usleep(50);
-		clearstatcache();
 		if($_SESSION['status'] != $status) {
 			break;
 		}
 	}
+	$_SESSION['status'] = $status;
 	header("content-type:application/json");
 	echo(json_encode($status));
 	flush();
 }spl_autoload_register('myloader');
 session_name('status');
-session_start();
-if(!array_key_exists('status', $_SESSION)) {
-	$_SESSION['status'] = array
-	(
-		'pin_2' =>array('pin_2' ,'None','N/A'),
-		'pin_3' =>array('pin_3' ,'None','N/A'),
-		'pin_4' =>array('pin_4' ,'None','N/A'),
-		'pin_7' =>array('pin_7' ,'None','N/A'),
-		'pin_8' =>array('pin_8' ,'None','N/A'),
-		'pin_9' =>array('pin_9' ,'None','N/A'),
-		'pin_10'=>array('pin_10','None','N/A'),
-		'pin_11'=>array('pin_11','None','N/A'),
-		'pin_14'=>array('pin_14','None','N/A'),
-		'pin_15'=>array('pin_15','None','N/A'),
-		'pin_17'=>array('pin_17','None','N/A'),
-		'pin_18'=>array('pin_18','None','N/A'),
-		'pin_22'=>array('pin_22','None','N/A'),
-		'pin_23'=>array('pin_23','None','N/A'),
-		'pin_24'=>array('pin_24','None','N/A'),
-		'pin_25'=>array('pin_25','None','N/A'),
-		'pin_27'=>array('pin_27','None','N/A')
-	);
-} 
+session_start(); 
 comet();
