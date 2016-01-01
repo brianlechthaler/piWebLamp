1d3ac5b26ecf2281a7650b8e855da9bff44e9fa7
root <root@mel-debian>
2015-12-31 19:02:39 -0800

    Added closing PHP tag to status_comet.php

--- weblamp/apache2/cgi-bin/status_comet.php
+++ weblamp/apache2/cgi-bin/status_comet.php
@@ -45,3 +45,19 @@ while ($_SESSION['status'] == $status) {
 }
 echo(json_encode($status));
 flush();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title></title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.26" />
+</head>
+
+<body>
+	
+</body>
+
+</html>
